bc9536aa04f0b4f4add2dac844bb9239013db3f2
chevalmuscle <me@marcantoinejean.com>
2020-09-08 21:52:34 -0400

    🛣 Added basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <div>Hello world !</div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/books">
+            <div>Books</div>
+          </Route>
+          <Route path="/">
+            <div>Home</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
